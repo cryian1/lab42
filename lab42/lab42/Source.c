028ea0456187699cab78dbb26e438c73ff374335
cryian1 <maxim75099@gmail.com>
2025-11-06 14:46:57 +0300

    4

--- lab42/lab42/Source.c
+++ lab42/lab42/Source.c
@@ -9,7 +9,30 @@ void main()
 	SetConsoleOutputCP(1251);
 	printf("¬ведите первое = ");
 	scanf_s("%d", &a);
-
+	//#include <stdio.h>
+//#include <Windows.h>
+//
+//void main()
+//{
+//	int a, b, min, max;
+//
+//	SetConsoleCP(1251);
+//	SetConsoleOutputCP(1251);
+//	printf("¬ведите первое число = ");
+//	scanf_s("%d", &a);
+//	printf("¬ведите второе число = ");
+//	scanf_s("%d", &b);
+//	if (a < b) {
+//		min = a;
+//		max = b;
+//	}
+//	else {
+//		min = b;
+//		max = a;
+//	}
+//	printf("большее число = %d\n", max);
+//	printf("меньшее число = %d\n", min);
+//}
 	scanf_s("%d", &b);
 	if (a < b) {
 		min = a;
