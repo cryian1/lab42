1866f4dde445a2a424477f7edbe1dfa5cb725286
cryian1 <maxim75099@gmail.com>
2025-11-06 14:32:12 +0300

    test1

---
+++ lab42/lab42/Source.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <Windows.h>
+
+void main()
+{
+	int a, b, min, max;
+
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	printf("¬ведите первое число = ");
+	scanf_s("%d", &a);
+	printf("¬ведите второе число= ");
+	scanf_s("%d", &b);
+	if (a < b) {
+		min = a;
+		max = b;
+	}
+	else {
+		min = b;
+		max = a;
+	}
+	printf("большее число = %d\n", max);
+	printf("меньшее число = %d\n", min);
+}
